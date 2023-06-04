648314c28ee205c31f00f5f73babb8164a494529
Kailash Bisht <krukailash8501@gmail.com>
2023-06-04 17:20:38 +0530

    (chore): add setup configurations for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "@tool-belt/eslint-config", "prettier"],
+	ignorePatterns: ['.eslintrc.js'],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+	settings: {
+		'import/resolver': {
+			typescript: {
+				project: '.',
+			},
+		},
+	},
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+		project: 'tsconfig.json',
+    sourceType: "module",
+  },
+  rules: {
+		'@typescript-eslint/no-extraneous-class': 0,
+		'@typescript-eslint/no-magic-numbers': 0,
+		'@typescript-eslint/no-unsafe-enum-comparison': 0,
+		'@typescript-eslint/naming-convention': [
+			'error',
+			{
+				selector: 'interface',
+				format: ['PascalCase'],
+				custom: {
+					regex: '(?!IOS)(^I[A-Z])',
+					match: false,
+				},
+			},
+		],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "consistent-return": "off",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "react/jsx-filename-extension": "off",
+    "prettier/prettier": "warn",
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <h1>k-trax</h1>
-  );
+	return <h1>k-trax</h1>;
 }
