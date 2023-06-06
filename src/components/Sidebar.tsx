4fd936cfbb84fc3224328d9a8278409b47268ad3
Kailash Bisht <krukailash8501@gmail.com>
2023-06-06 23:32:25 +0530

    (chore): format

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import {
+	Box,
+	Center,
+	Divider,
+	LinkBox,
+	LinkOverlay,
+	List,
+	ListIcon,
+	ListItem,
+} from '@chakra-ui/layout';
+
+export default function Sidebar() {
+	return <div>Sidebar</div>;
+}
