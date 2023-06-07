a70452a7d1565c1ee61647490ceea4dce55c26b3
Kailash Bisht <krukailash8501@gmail.com>
2023-06-08 03:07:17 +0530

    (chore): format cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
