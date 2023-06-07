3a42d38352754d240b76f21a3be62cd8352c0011
Kailash Bisht <krukailash8501@gmail.com>
2023-06-07 12:57:55 +0530

    (fix): fix vitest setup

--- tests/pages/home.spec.tsx
+++ tests/pages/home.spec.tsx
@@ -1,16 +1,11 @@
 import { expect, describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Home from '@/src/pages';
 
 describe('home', () => {
-  it('tests home page', () => {
-    render(<Home />);
-    expect(
-      screen.getByRole('heading', { level: 1, name: /welcome to next\.js!/i }),
-    ).toBeDefined();
-
-    const footer = screen.getByRole('contentinfo');
-    const link = footer.getByRole('link');
-    expect(link.getByRole('img', { name: /vercel logo/i })).toBeDefined();
-  })
+	it('tests home page', () => {
+		render(<Home />);
+		const text = screen.getByText('k-trax');
+    expect(text).toBeInTheDocument();
+	});
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 	test: {
 		environment: 'jsdom',
 		include: ['**/*.spec.tsx'],
-    setupFiles: ['./tests/setup.ts'],
-    globals: true
+		setupFiles: ['./tests/setup.ts'],
+		globals: true,
 	},
 });
