03fc248527525713036aa574b9d453b548288bfe
Kailash Bisht <krukailash8501@gmail.com>
2023-06-07 13:28:50 +0530

    (chore): add unit tests for Nav menu

---
+++ tests/components/NavMenu.spec.tsx
@@ -0,0 +1,24 @@
+import NavMenu from "@/src/components/NavMenu";
+import { NAV_MENU } from "@/src/components/constants";
+import { render, screen } from "@testing-library/react";
+
+describe('NavMenu', () => {
+  const [home, search, library] = NAV_MENU;
+  it('renders home tab in nav menu', () => {
+    render(<NavMenu menu={NAV_MENU} />);
+    const homeText = screen.getByText(home.name);
+    expect(homeText).toBeInTheDocument();
+  });
+
+  it('renders search tab in nav menu', () => {
+    render(<NavMenu menu={NAV_MENU} />);
+    const searchText = screen.getByText(search.name);
+    expect(searchText).toBeInTheDocument();
+  });
+
+  it('renders library tab in nav menu', () => {
+    render(<NavMenu menu={NAV_MENU} />);
+    const libraryText = screen.getByText(library.name);
+    expect(libraryText).toBeInTheDocument();
+  });
+});
