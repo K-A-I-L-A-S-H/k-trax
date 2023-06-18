9e009e5fd1c2eaaad7fe783ce3e871dff2a4e6fa
Kailash Bisht <krukailash8501@gmail.com>
2023-06-18 21:44:58 +0530

    (chore): add custom hook for auth iam and playlists

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -2,5 +2,21 @@ import useSWR from 'swr';
 import fetcher from './fetcher';
 
 export function useMe() {
-	const {} = useSWR('/me', fetcher);
+	const { data, error } = useSWR('/auth/me', fetcher);
+
+	return {
+		user: data,
+		isLoading: !data && !error,
+		isError: error,
+	};
+}
+
+export function usePlaylist() {
+	const { data, error } = useSWR('/platlist', fetcher);
+
+	return {
+		playlists: data || [],
+		isLoading: !data && !error,
+		isError: error,
+	};
 }
