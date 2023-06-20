b9e0eeffbfaabfd6dc7b776236427d2efc27ba50
Kailash Bisht <krukailash8501@gmail.com>
2023-06-21 00:03:39 +0530

    (chore): add dynamic playlist count using custom hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home(
 			color="teal"
 			subtitle="profile"
 			title={`${user.firstName} ${user.lastName}`}
-			description="10 public playlists"
+			description={`${user.playlistsCnt} public playlists`}
 			image="https://img.freepik.com/free-vector/man-face-close-up_98292-4059.jpg?w=1060&t=st=1687112927~exp=1687113527~hmac=7aa67d37b7546b44adf8f3ccd08e53dd660399f4b5aea49c5f4acb7930b16ba0"
 			roundImage
 		>
