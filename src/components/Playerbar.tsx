d7c3857f7b3e849e6e2ab7fa20335b4c889473d6
Kailash Bisht <krukailash8501@gmail.com>
2023-06-22 16:35:27 +0530

    (chore): cleanup format

--- src/components/Playerbar.tsx
+++ src/components/Playerbar.tsx
@@ -1,13 +1,13 @@
 import { Box, Flex } from '@chakra-ui/layout';
 import PlayerAlbumTile from './PlayerAlbumTile';
-import PlayerControl from './PlayerControl';
+import Player from './Player';
 
 export default function PlayerBar() {
 	return (
 		<Box height="100px" width="100vw" bg="gray.900" padding="10px">
 			<Flex align="center">
 				<PlayerAlbumTile />
-				<PlayerControl />
+				<Player />
 			</Flex>
 		</Box>
 	);
